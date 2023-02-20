3b07a6a960a8dff073a3f627358e11a29482007a
Samarth Gupta <samarthgupta1202@gmail.com>
2023-02-20 22:34:39 +0530

    header created

---
+++ meme_generator/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header className="header">
+            <img src={require("./troll.png")} alt="trollface" className="header-image" />
+            <h2 className="header-title">Meme Generator</h2>
+            <h4 className="header-project">React course project - 3</h4>
+        </header>
+    )
+
+
+}
+
+export default Header
