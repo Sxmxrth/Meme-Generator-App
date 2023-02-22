3bccc3871c200c2c8e46dfbcbea636bb33310527
Samarth Gupta <samarthgupta1202@gmail.com>
2023-02-22 23:22:26 +0530

    Random meme link alerted

--- meme_generator/src/components/Meme.jsx
+++ meme_generator/src/components/Meme.jsx
@@ -1,12 +1,21 @@
 import React from "react"
+import memesData from "../memeData.js"
 
 function Meme() {
+
+    function generateImg() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        alert(url)
+    }
+
     return (
         <main>
             <form className="form">
                 <input type="text" className="form--input" placeholder="Top text" />
                 <input type="text" className="form--input" placeholder="Bottom text " />
-                <button type="submit" className="form--button">Get a new meme image</button>
+                <button type="submit" className="form--button" onClick={generateImg}>Get a new meme image</button>
             </form>
         </main>
 
