a64f7289ec8de20afbdbede4ea2f2f532f865500
Samarth Gupta <samarthgupta1202@gmail.com>
2023-02-22 22:47:44 +0530

    meme component partially built

--- meme_generator/src/App.js
+++ meme_generator/src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "./components/Header.jsx"
+import Meme from "./components/Meme.jsx"
 import "./styles.css"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
